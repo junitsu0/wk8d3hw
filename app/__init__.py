d4a86f0bc8ae2fc1e36d2753c3c8950c77dd6163
Justin <junitsu00@gmail.com>
2022-10-26 12:32:06 -0500

    do the thing

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Create an instance of SQLAlchemy (the ORM) with the Flask Application
+db = SQLAlchemy(app)
+# Create an instance of Migrate which will be our migration engine and pass in the app and SQLAlchemy instance
+migrate = Migrate(app, db)
+# Create an instance of the LoginManager to handle authentication
+login = LoginManager(app)
+login.login_view = 'login' # Tells the login manager which endpoint to redirect if someone if not logged in
+login.login_message = 'You must be logged in to do that you silly goose'
+login.login_message_category = 'danger'
+
+
+from . import routes, models
+
